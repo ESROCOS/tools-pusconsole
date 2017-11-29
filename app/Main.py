da8a80e0097d116d6ef64ca1a04385d12ef40736
Javier Román <jroman@gmv.com>
2017-11-29 10:09:36 +0100

    Second commit

--- app/Main.py
+++ app/Main.py
@@ -5,13 +5,4 @@ import sys
 
 gui = QtGui.QApplication(sys.argv)
 app = App()
-args = sys.argv()
-
-if args[1] == "-test":
-    with open('tm1.json') as json_data:
-        d = json.load(json_data)
-        app.add(d)
-    with open('tm2.json') as json_data:
-        d = json.load(json_data)
-        app.add(d)
 sys.exit(gui.exec_())
